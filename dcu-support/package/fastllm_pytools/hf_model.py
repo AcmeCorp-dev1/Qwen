9497aec4b42fcabd7866b5772581ce39e965af23
Rahul-Alladi <130827448+Rahul-Alladi@users.noreply.github.com>
2024-05-25 02:15:42 +0530

    Update hf_model.py

--- dcu-support/package/fastllm_pytools/hf_model.py
+++ dcu-support/package/fastllm_pytools/hf_model.py
@@ -56,6 +56,12 @@ fastllm_data_type_dict = {
 # TODO bfloat
 # TODO bfloat
 # TODO bfloat
+# TODO bfloat
+# TODO bfloat
+# TODO bfloat
+# TODO bfloat
+# TODO bfloat
+# TODO bfloat
 fastllm_weight_type_dict = {
     "linear": 1,
     "embedding": 2,
